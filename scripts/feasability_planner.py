5edea323388b4f8110918b0c1207b2a2e29934b2
Imdimark <78663960+Imdimark@users.noreply.github.com>
2021-05-31 16:45:08 +0200

    Add files via upload

--- scripts/feasability_planner.py
+++ scripts/feasability_planner.py
@@ -1,5 +1,4 @@
-
-#!/usr/bin/env python  
+#!/usr/bin/env python
 import rospy
 
 # Because of transformations
@@ -16,37 +15,63 @@ blocks_id=['C', 'E', 'G','I', 'M', 'A', 'B', 'D', 'F', 'H', 'L']
 client_trans=None
 pub=None
 
+def distance_between_blocks(f_block, s_block, threshold):
+    dis=sqrt((f_block.transform.transform.translation.x - s_block.transform.transform.translation.x )**2 + (f_block.transform.transform.translation.y - s_block.transform.transform.translation.y )**2)
+    if dis < threshold:
+        if f_block.transform.transform.translation.z <= s_block.transform.transform.translation.z:
+            return True
+        else: 
+            return False
+    else:
+        False
+
 if __name__ == '__main__':
-        global blocks_state, blocks_id
-        global client_trans, pub
-	rospy.init_node('feasability_planner')
-	client_trans= rospy.ServiceProxy('transform', Transformation)   
-        pub = rospy.Publisher('blocks_state', BlocksState , queue_size=1)
-        message=Transformation()        
-        crowded=False
-	rate= rospy.Rate(20)
-	
-	while not rospy.is_shutdown():
-	           for i in range(0:5)
-	                message.frame_id=blocks_id[i]
-	                message.transform=client_trans(message.frame_id)
-	                for j in range (0:11):
-	                    if i!=j:
-	                         crowded=distance_between_blocks(blocks_id[i],blocks_id[j])
-	                         if crowded==True:
-	                             blocks_state[i]=4;
-	                             break;
-	                     
-			if message.transform.transform.translation.x>0:
-				blocks_state[i]=1
-				             
-			if message.transform.transform.translation.x==0:
-			        blocks_state[i]=2
-				             
-				##setto parametro middleplacent true RICORDA
-			if message.transform.transform.translation.x<0:
-				blocks_state[i]=3
-				             
-			se (x,y)==bluebox
-				blocks_state[i]=3 
-                   rate.sleep()
+
+    global blocks_state, blocks_id
+    global client_trans, pub
+
+    rospy.init_node('feasability_planner')
+    client_trans= rospy.ServiceProxy('transform', Transformation)
+    pub = rospy.Publisher('blocks_state', BlocksState , queue_size=1)
+    state_message = BlocksState()
+    blue_box_transformation = Trasformation()
+    referred_block=Transformation()
+    other_block=Transformation()
+    crowded=False
+    rate= rospy.Rate(20)
+    blue_box_transformation.frame_id= 'BlueBox'
+    blue_box_transformation.transform=client_trans(blue_box_transformation.frame_id)
+
+    while not rospy.is_shutdown():
+        
+        for i in range(0:5)
+
+            referred_block.frame_id=blocks_id[i]
+            referred_block.transform=client_trans(referred_block.frame_id)
+
+            for j in range (0:11):
+                if i!=j:
+                    other_block.transform=client_trans(blocks_id[j])
+                    crowded=distance_between_blocks(referred_block.transform, other_block.transform, 2.5)
+                if crowded==True:
+                    blocks_state[i]=4
+                    break
+
+            if referred_block.transform.transform.translation.x>0:
+                blocks_state[i]=1
+                
+            if referred_block.transform.transform.translation.x==0:
+                blocks_state[i]=2
+            ##setto parametro middleplacent true RICORDA
+
+            if referred_block.transform.transform.translation.x<0:
+                blocks_state[i]=3
+
+            if  (referred_block.transform, blue_box.transformation.transform, 10) = True: 
+                blocks_state[i]=0
+        
+        state_message.blocksarray= blocks_state
+        pub.publish(state_message)
+
+        rate.sleep()
+    
